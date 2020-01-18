f132b0d9772e612f2bb87661843acec14ff6c0a6
broortiz93 <59903499+broortiz93@users.noreply.github.com>
2020-01-17 19:26:30 -0600

    Update StoreProcedure.java

--- src/main/java/com/javanes/framework/persistence/StoreProcedure.java
+++ src/main/java/com/javanes/framework/persistence/StoreProcedure.java
@@ -1,18 +1,16 @@
 package com.javanes.framework.persistence;
 
-import java.io.ByteArrayInputStream;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-
 import javax.annotation.Resource;
 import javax.sql.DataSource;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -23,19 +21,22 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Repository;
-
 import com.javanes.framework.model.EntityBase;
+import com.javanes.framework.model.PagoServicio;
 import org.springframework.jdbc.core.SqlReturnResultSet;
-
 
 @Repository
 public class StoreProcedure  {
 	private JdbcTemplate jdbcTemplate;
 	private String BDIsac="bdisac:informix";
 	private String BDInteg="bdinteg:informix";
+	private String BDIbpi="bdibpi:informix";
 	private String spParametro="sp_obtieneparametro";
+	private String spConsultaConvenio="sp_consulta_convenio";
+	private String spObtenerNumTrans="sp_obtenernumtrans";
 	private String spfecha="sp_obtener_fecha";
 	private String spPagoServicio="sp_pago_servicios";
+	
 	  private static final Logger LOG = LoggerFactory
 		      .getLogger(StoreProcedure.class);
 
@@ -43,7 +44,6 @@ public class StoreProcedure  {
 	public final void setDataSource(final DataSource dataSource) {
 	       this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
-
 	
 	public EntityBase getParams(Integer id) throws Exception {
 		try {
@@ -92,8 +92,6 @@ public class StoreProcedure  {
 
 			SqlParameterSource in = new MapSqlParameterSource().addValue("siParametro", id);
 			Map<String, Object> out = jdbcCall.execute(in);
-			
-			
 			
 			EntityBase student = new EntityBase();
 			student.setCodReturn((String) out.get("cCodret"));
@@ -150,7 +148,13 @@ public class StoreProcedure  {
 		
 	}
 	
-	public EntityBase spPagoServicios() throws Exception {
+	public EntityBase spPagoServicios(PagoServicio pagoServicio) throws Exception {
+		
+		LOG.info("Ejecutando: bdisac:sp_pago_servicios");
+		
+		PagoServicio resObtieneParametro = new PagoServicio();
+		PagoServicio resObtNumTransferencia = new PagoServicio();
+		
 		try {
 			SimpleJdbcCall jdbcCall = new 
 					SimpleJdbcCall(jdbcTemplate).withSchemaName(BDIsac).withProcedureName(spPagoServicio).declareParameters(
@@ -179,28 +183,126 @@ public class StoreProcedure  {
 			 }));
 			 
 			jdbcCall.compile();
-
 			
-			SqlParameterSource in = new MapSqlParameterSource().addValue("pEmpresa", "001").addValue("pSucursal", "5003").addValue("pUsuario", "transBPI")
-					.addValue("pTransCargo", "1137").addValue("pTransAbono", "1167").addValue("pTransSuc", "8651")
-					.addValue("pFolioSuc", "0158467718274728").addValue("pNumCtaOrigen", "12000872207").addValue("pNumCtaDestino", "99010000056")
-					.addValue("pCheque", 0).addValue("pMonto", 299.00).addValue("pMoneda", "01")
-					.addValue("pReferencia", "501177161582 VETEVE").addValue("pNumTarjetaOrigen", "").addValue("pNumTarjetaDestino", "")
-					.addValue("pUsuAutoriza", "").addValue("pMontoTotal", 299.00).addValue("pMontoFirme", 299.00)
+			resObtieneParametro = obtieneParametro(pagoServicio.getCuentaServicio());
+			resObtNumTransferencia = obtieneNumeroTransferencia(pagoServicio.getOperacionServicio());
+			
+			String pNumEmpresa="001";
+			String pSucursal="5003";
+			String pUsuario="transBPI";
+			String folio = generaFolio("1584677");
+			String moneda = "01";
+			String numVerificador = pagoServicio.getcRefTelefono().substring(pagoServicio.getcRefTelefono().length() - 1);
+			
+			SqlParameterSource in = new MapSqlParameterSource().addValue("pEmpresa", pNumEmpresa).addValue("pSucursal", pSucursal).addValue("pUsuario", pUsuario)
+					.addValue("pTransCargo", resObtNumTransferencia.getpTransCargo()).addValue("pTransAbono", resObtNumTransferencia.getpTransAbono()).addValue("pTransSuc", pagoServicio.getpTransSuc())
+					.addValue("pFolioSuc", folio).addValue("pNumCtaOrigen", pagoServicio.getpNumCtaOrigen()).addValue("pNumCtaDestino", resObtieneParametro.getpNumCtaDestino())
+					.addValue("pCheque", 0).addValue("pMonto", pagoServicio.getpMonto()).addValue("pMoneda", moneda)
+					.addValue("pReferencia", pagoServicio.getpReferencia()).addValue("pNumTarjetaOrigen", "").addValue("pNumTarjetaDestino", "")
+					.addValue("pUsuAutoriza", "").addValue("pMontoTotal", pagoServicio.getpMonto()).addValue("pMontoFirme", pagoServicio.getpMonto())
 					.addValue("pMontoSBC", 0).addValue("pMontoRem", 0).addValue("pDiasRet", 0)
-					.addValue("pDocto", 0).addValue("pCategoria", "06").addValue("pConvenio", "001")
-					.addValue("cRefTelefono", "501177161582").addValue("cRefVerificador", "2").addValue("cFormaPago", "2")
-					.addValue("cCuentaCargo", "12000872207").addValue("cTransacc_suc", "8651").addValue("dFechaPago", new Date("2020/01/16"));
+					.addValue("pDocto", 0).addValue("pCategoria", pagoServicio.getpCategoria()).addValue("pConvenio", pagoServicio.getpConvenio())
+					.addValue("cRefTelefono", pagoServicio.getcRefTelefono()).addValue("cRefVerificador", numVerificador).addValue("cFormaPago", "2")
+					.addValue("cCuentaCargo", pagoServicio.getpNumCtaOrigen()).addValue("cTransacc_suc", pagoServicio.getpTransSuc()).addValue("dFechaPago", new Date(pagoServicio.getdFechaPago()));
 			Map<String, Object> out = jdbcCall.execute(in);
-
+			LOG.info("Trama: "+jdbcCall.getCallString());
 			out.entrySet().forEach(System.out::println);
+			
+			final Map<String, Object> resultMap = (Map<String, Object>) out.get("result");
 			EntityBase student = new EntityBase();
-			student.setCodReturn((String) out.get("cCodret"));
-			student.setCodValor((String) out.get("cValor"));
+			student.setCodReturn(resultMap.get("vcodret").toString().trim());
+			student.setCodValor(resultMap.get("vcodretRev").toString().trim());
 			return student;      
 		} catch (Exception e) {
 			LOG.info("intentando getParams "+e);
 			throw new Exception("Error al ejecutar SP");
 		}
+	}
+	
+	private PagoServicio obtieneParametro(Integer cuentaServicio) throws Exception {
+		PagoServicio psParam = new PagoServicio();
+		try {
+			SimpleJdbcCall jdbcCall = new 
+					SimpleJdbcCall(jdbcTemplate).withSchemaName(BDIsac).withProcedureName(spParametro).declareParameters(
+		                      new SqlParameter("siParametro", Types.INTEGER));
+			jdbcCall.declareParameters(new SqlReturnResultSet("result", new ResultSetExtractor<Map<String, Object>>() {
+				@Override
+				public  Map<String, Object>  extractData(ResultSet resultSet) throws SQLException {
+					Map<String, Object> map = null;
+			            
+			        while(resultSet.next()) {
+			        	map = new HashMap<String, Object>();
+			            map.put("cCodret", resultSet.getString(1));
+			            map.put("cValor", resultSet.getString(2));
+			        }
+			        return map;
+			    }
+			 }));
+			jdbcCall.compile();
+			SqlParameterSource in = new MapSqlParameterSource().addValue("siParametro", cuentaServicio);
+			Map<String, Object> out = jdbcCall.execute(in);
+			out.entrySet().forEach(System.out::println);
+			
+			final Map<String, Object> resultMap = (Map<String, Object>) out.get("result");
+			final String codRet = resultMap.get("cCodret").toString().trim();
+			final String cValor = resultMap.get("cValor").toString().trim();
+			
+			psParam.setpCodRetorno(codRet);
+			psParam.setpNumCtaDestino(cValor);
+			
+			return psParam;
+		} catch (Exception e) {
+			LOG.info("intentando getParams "+e);
+			throw new Exception("Error al ejecutar SP sp_obtieneparametro");
+		}
+	}
+	
+	private PagoServicio obtieneNumeroTransferencia(String idOperacion) throws Exception {
+		PagoServicio psParam = new PagoServicio();
+		try {
+			SimpleJdbcCall jdbcCall = new 
+					SimpleJdbcCall(jdbcTemplate).withSchemaName(BDIbpi).withProcedureName(spObtenerNumTrans).declareParameters(
+		                      new SqlParameter("pIdOpe", Types.INTEGER));
+			jdbcCall.declareParameters(new SqlReturnResultSet("result", new ResultSetExtractor<Map<String, Object>>() {
+				@Override
+				public  Map<String, Object>  extractData(ResultSet resultSet) throws SQLException {
+					Map<String, Object> map = null;
+			            
+			        while(resultSet.next()) {
+			        	map = new HashMap<String, Object>();
+			            map.put("vCod_Ret", resultSet.getString(1));
+			            map.put("vIdTran", resultSet.getString(2));
+			        }
+			        return map;
+			    }
+			 }));
+			jdbcCall.compile();
+			SqlParameterSource in = new MapSqlParameterSource().addValue("pIdOpe", idOperacion);
+			Map<String, Object> out = jdbcCall.execute(in);
+			out.entrySet().forEach(System.out::println);
+			
+			final Map<String, Object> resultMap = (Map<String, Object>) out.get("result");
+			final String codRet = resultMap.get("vCod_Ret").toString().trim();
+			final String vIdTrans = resultMap.get("vIdTran").toString().trim();
+			
+			psParam.setpCodRetorno(codRet);
+			psParam.setpTransCargo(vIdTrans.substring(0, 4));
+			psParam.setpTransAbono(vIdTrans.substring(4));
+			
+			return psParam;
+		} catch (Exception e) {
+			LOG.info("intentando obtenerNumTransfer "+e);
+			throw new Exception("Error al ejecutar SP sp_obtenernumtrans");
+		}
+	}
+	
+	public static String generaFolio(final String idCliente) {
+		Date fechaHoy = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("HHmmssSS");
+		StringBuilder sb= new StringBuilder();
+		sb.append(String.format("%08d", Integer.parseInt(idCliente)));
+		sb.append(sdf.format(fechaHoy).substring(0, 8));
+		return sb.toString();
+		
 	}
 }
